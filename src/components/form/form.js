81113a8fc5037719371eccbb37572a60e36b0ce6
Jason Taisey <jdtaisey89@icloud.com>
2021-05-31 14:16:44 -0700

    Pushing up a somewhat working page

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -36,15 +36,15 @@ class Form extends React.Component {
       <div id="form">
         <form onSubmit={this.handleSubmit} value={this.props.route}> 
         <section>
-          <label>Paste in the URL:</label>
-          <input type="text" value={this.props.url} onChange={this.props.handleChange} placeholder=""/>
-          <button type="submit">Run</button>
+          <h3 id="info">Paste in the URL:</h3>
+          <input id="inputUrl" type="text" value={this.props.url} onChange={this.props.handleChange} placeholder=""/>
+          <button id="run" type="submit">Run</button>
         </section>
         <section>
-          <button onClick={this.props.handleClick} className="rest-action" value="GET">GET</button>
-          <button onClick={this.props.handleClick} className="rest-action" value="PUT">PUT</button>
-          <button onClick={this.props.handleClick} className="rest-action" value="POST">POST</button>
-          <button onClick={this.props.handleClick} className="rest-action" value="DELETE">DELETE</button>
+          <button onClick={this.props.handleClick} className="rest-action-1" value="GET">GET</button>
+          <button onClick={this.props.handleClick} className="rest-action-2" value="PUT">PUT</button>
+          <button onClick={this.props.handleClick} className="rest-action-3" value="POST">POST</button>
+          <button onClick={this.props.handleClick} className="rest-action-4" value="DELETE">DELETE</button>
         </section>
         <section>
           <input type="text" className="query-parameters" placeholder="enter in query parameters if applicable"/>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import './header.scss'
 import { NavLink } from 'react-router-dom';
+import { render } from '@testing-library/react';
 
-const Header = () => {
-  return (
-    <div id="header">
-      <link rel="preconnect" href="https://fonts.gstatic.com"/>
-      <link href="https://fonts.googleapis.com/css2?family=Limelight&display=swap" rel="stylesheet"></link>
-      <header>
-      <h1 id="restHeader">RESTy</h1>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/">History</NavLink>
-          </li>
-          <li>
-            <NavLink to="/">Help</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-    </div>
-  )
+class Header extends React.Component{
+  render (){
+    return(
+      <div id="header">
+        <header>
+          RESTy
+        </header>
+        <nav>
+          <ul>
+            <li className="liHeader">
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li className="liHeader">
+              <NavLink to="/history">History</NavLink>
+            </li>
+            <li className="liHeader">
+              <NavLink to="/help">Help</NavLink>
+            </li>
+
+          </ul>
+        </nav>
+      </div>
+    )
+  }
 }
 
 export default Header;
